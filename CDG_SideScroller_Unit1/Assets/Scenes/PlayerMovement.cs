16615552283ff84a7fb5ae58f145b1bb107c1931
Craig Granger <10992413@uvu.edu>
2023-09-01 17:24:20 -0600

    Player Movement Updated
    Fixed player rotating by freezing z axis - player can stick to walls and climb - need to repair this :+1:

--- CDG_SideScroller_Unit1/Assets/Scenes/PlayerMovement.cs
+++ CDG_SideScroller_Unit1/Assets/Scenes/PlayerMovement.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 10f;
+    public float jumpForce = 15f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+
+    private float moveDirection;
+    private bool isGrounded;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        // Handle movement
+        float moveDirection = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
         
+        
+
+        // Handle jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
